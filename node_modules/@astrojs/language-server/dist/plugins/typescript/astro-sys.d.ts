b0adf16b1ec1c55ee684215ea8b37e972bc4271f
MostlyWhat <78652091+MostlyWhat@users.noreply.github.com>
2021-12-18 19:50:58 +0700

    Delete node_modules directory

--- node_modules/@astrojs/language-server/dist/plugins/typescript/astro-sys.d.ts
+++
@@ -1,6 +0,0 @@
-import * as ts from 'typescript';
-import { DocumentSnapshot } from './DocumentSnapshot';
-/**
- * This should only be accessed by TS Astro module resolution.
- */
-export declare function createAstroSys(getSnapshot: (fileName: string) => DocumentSnapshot): ts.System;
