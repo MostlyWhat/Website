bc5bc18d4e2580e37a154828918908278e8969c5
MostlyWhat <mostlywhatstudios@gmail.com>
2022-02-13 21:12:10 +0700

    Added Contact Page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
     content: [
         './public/**/*.html',
+        './node_modules/flowbite/**/*.js',
         './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {},
