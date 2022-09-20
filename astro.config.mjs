ff69179d619d50f2dba34574fdcd2ba32e4e8567
MostlyWhat <mostlywhatstudios@gmail.com>
2022-09-20 16:16:08 +0700

    Added Coming Soon Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), image()]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}',
-		"./node_modules/flowbite/**/*.js",
-	],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [
-        require('flowbite/plugin')
-    ],
+	plugins: [],
 }
