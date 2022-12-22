a51ce9f7d688f55db1d3433d3d36c4b52d2b510b
MostlyWhat <mostlywhatstudios@gmail.com>
2022-12-22 11:36:45 +0700

    Updated to Dark Mode on Base, Header and Footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -51,38 +51,9 @@ module.exports = {
 				warning: '#FF6700',
 				danger: '#DD1C1A',
 				bg_dark: '#000814',
-				bg_light: '#FFFFFF'
-			},
-			fontFamily: {
-				sans: ["'InterVariable'", ...defaultTheme.fontFamily.sans],
 			},
 		},
 	},
 	plugins: [require('@tailwindcss/typography')],
 	darkMode: 'class',
 };
-
-/* 
-
-  Alternative tailwind.config.js
-  
-  NOTE: Add this fonts to <head>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap" rel="stylesheet" />
-*/
-
-// module.exports = {
-//   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-//   theme: {
-//     extend: {
-//       colors: {
-//         primary: colors.cyan,
-//         secondary: colors.lime,
-//       },
-//       fontFamily: {
-//         sans: ["'Nunito'", ...defaultTheme.fontFamily.sans],
-//       },
-//     },
-//   },
-//   plugins: [require("@tailwindcss/typography")],
-//   darkMode: "class",
-// };
