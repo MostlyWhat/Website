efd02a8b439e8b499b0fed2a35937a276fd799cc
MostlyWhat <mostlywhatstudios@gmail.com>
2023-02-18 14:42:18 +0700

    Formatted Files

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -1,20 +1,15 @@
 declare module 'astro:content' {
 	export { z } from 'astro/zod';
-	export type CollectionEntry<C extends keyof typeof entryMap> =
-		(typeof entryMap)[C][keyof (typeof entryMap)[C]] & Render;
+	export type CollectionEntry<C extends keyof typeof entryMap> = (typeof entryMap)[C][keyof (typeof entryMap)[C]] &
+		Render;
 
 	type BaseSchemaWithoutEffects =
 		| import('astro/zod').AnyZodObject
 		| import('astro/zod').ZodUnion<import('astro/zod').AnyZodObject[]>
 		| import('astro/zod').ZodDiscriminatedUnion<string, import('astro/zod').AnyZodObject[]>
-		| import('astro/zod').ZodIntersection<
-				import('astro/zod').AnyZodObject,
-				import('astro/zod').AnyZodObject
-		  >;
+		| import('astro/zod').ZodIntersection<import('astro/zod').AnyZodObject, import('astro/zod').AnyZodObject>;
 
-	type BaseSchema =
-		| BaseSchemaWithoutEffects
-		| import('astro/zod').ZodEffects<BaseSchemaWithoutEffects>;
+	type BaseSchema = BaseSchemaWithoutEffects | import('astro/zod').ZodEffects<BaseSchemaWithoutEffects>;
 
 	type BaseCollectionConfig<S extends BaseSchema> = {
 		schema?: S;
@@ -26,24 +21,17 @@ declare module 'astro:content' {
 			data: import('astro/zod').infer<S>;
 		}) => string | Promise<string>;
 	};
-	export function defineCollection<S extends BaseSchema>(
-		input: BaseCollectionConfig<S>
-	): BaseCollectionConfig<S>;
+	export function defineCollection<S extends BaseSchema>(input: BaseCollectionConfig<S>): BaseCollectionConfig<S>;
 
 	type EntryMapKeys = keyof typeof entryMap;
 	type AllValuesOf<T> = T extends any ? T[keyof T] : never;
 	type ValidEntrySlug<C extends EntryMapKeys> = AllValuesOf<(typeof entryMap)[C]>['slug'];
 
-	export function getEntryBySlug<
-		C extends keyof typeof entryMap,
-		E extends ValidEntrySlug<C> | (string & {})
-	>(
+	export function getEntryBySlug<C extends keyof typeof entryMap, E extends ValidEntrySlug<C> | (string & {})>(
 		collection: C,
 		// Note that this has to accept a regular string too, for SSR
 		entrySlug: E
-	): E extends ValidEntrySlug<C>
-		? Promise<CollectionEntry<C>>
-		: Promise<CollectionEntry<C> | undefined>;
+	): E extends ValidEntrySlug<C> ? Promise<CollectionEntry<C>> : Promise<CollectionEntry<C> | undefined>;
 	export function getCollection<C extends keyof typeof entryMap, E extends CollectionEntry<C>>(
 		collection: C,
 		filter?: (entry: CollectionEntry<C>) => entry is E
@@ -65,9 +53,7 @@ declare module 'astro:content' {
 		}>;
 	};
 
-	const entryMap: {
-		
-	};
+	const entryMap: {};
 
-	type ContentConfig = typeof import("../src/content/config");
+	type ContentConfig = typeof import('../src/content/config');
 }
