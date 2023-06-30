bb590f52a22cca45abcd1080f0a5418d3427e167
MostlyWhat <mostlywhatstudios@gmail.com>
2023-06-30 17:09:25 +0700

    Formatting Files

--- src/pages/pagefind/pagefind.js.ts
+++ src/pages/pagefind/pagefind.js.ts
@@ -1,7 +1,7 @@
-import type { APIContext } from "astro"
+import type { APIContext } from 'astro';
 
 export async function get(_: APIContext) {
-  return {
-    body: 'export const search = () => {return {results: []}}'
-  }
+	return {
+		body: 'export const search = () => {return {results: []}}',
+	};
 }
