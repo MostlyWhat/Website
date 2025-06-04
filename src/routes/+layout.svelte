a57ac143c95af24e8d4e40f7f0b3e5953f58c187
Pongsupa Supapa <mostlywhatstudios@gmail.com>
2025-06-03 19:43:26 -0500

    feat: styles and fonts update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,11 @@
 <script lang="ts">
+	// Fonts
+	import '@fontsource-variable/jetbrains-mono'; // Supports weights 100-800
+	import '@fontsource-variable/hubot-sans'; // Supports weights 200-900
+	import '@fontsource/chakra-petch';
+	import '@fontsource/tourney/900.css';
+
+	// Styles
 	import '../app.css';
 
 	let { children } = $props();
