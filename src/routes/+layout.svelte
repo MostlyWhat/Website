20255a9ccaa51315d9b24eb0e8c42dbaecb44fff
Pongsupa Supapa <mostlywhatstudios@gmail.com>
2025-08-19 10:30:06 -0500

    feat: updated styles and fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import '../app.css';
+	import '@fontsource-variable/tourney'; // 200-900
+	import '@fontsource-variable/hubot-sans'; // 200-900
+	import '@fontsource/chakra-petch'; // 200-900
+	import '@fontsource-variable/jetbrains-mono'; // 100-800
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
