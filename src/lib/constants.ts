4e5809883b089d75edfdddd0eb0d97ade5e0e995
Pongsupa Supapa <mostlywhatstudios@gmail.com>
2025-11-29 20:52:39 -0600

    style: fix indentation for MARATHON_VIDEO constant in constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@
 
 // Marathon video URL for hero backgrounds - persists across page transitions
 export const MARATHON_VIDEO: string =
-	'https://www.marathonthegame.com/content/dam/goliath/marathon/videos/33850_BNG_GO_BASE_TR_GP-REVEAL_V1_LF_4K_16x9_TL_NR_AGN_NA_NoSound.mp4';
+    'https://www.marathonthegame.com/content/dam/goliath/marathon/videos/33850_BNG_GO_BASE_TR_GP-REVEAL_V1_LF_4K_16x9_TL_NR_AGN_NA_NoSound.mp4';
 
 // Header heights for viewport calculations
 export const HEADER_HEIGHT = 64; // h-16 in pixels
